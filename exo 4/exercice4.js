802e02c25f58a67a8925543aca829f93501df83c
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-15 17:43:40 +0100

    exo 61

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,9 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 var i = 15;
-while (i   ) {
+for (i < 20; i++){
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    =   ;
+    i++;
 }
 
 
